effc465f3c4e21f5fe455b3591431a8890b32738
Sinux <aung.atg1738@gmail.com>
2023-12-22 01:16:12 +0630

    UploadV1

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,7 +5,7 @@ const Summary = () => {
     <section className='summary' >
       <h2>Best Shopping Website In Myanmar</h2>
       <p>
-      Responsive web design is an approach aimed at crafting websites to provide an optimal viewing and interaction experience across a wide range of devices. This technique ensures that users can easily navigate and access content regardless of whether they're using a desktop, tablet, or smartphone. By utilizing fluid grids, flexible images, and media queries, websites can dynamically adjust their layout and design, adapting seamlessly to different screen sizes and orientations. This adaptability not only enhances user experience but also acknowledges the diversity of devices people use to browse the internet today
+      Responsive web design is an approach aimed at crafting websites to provide an optimal viewing and interaction experience across a wide range of devices.
       </p>
     </section>
   )
